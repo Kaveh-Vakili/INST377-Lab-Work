61149534a4ef5ad3022d7ff27705e64ff98359db
MasterKaveman <50510650+Kaveh-Vakili@users.noreply.github.com>
2021-09-29 23:06:39 -0400

    added platforms

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -3,12 +3,23 @@
 
 document.addEventListener('DOMContentLoaded',()=>{
 const grid=document.querySelector('.grid')
-    const doodler=document.createElement('div')
+const doodler=document.createElement('div')
 let doodlerLeftSpace=50
 let doodlerBottomSpace=150
+let speed=3
 let isGameOver=false
 let platformCount=5
 let platforms=[]
+const gravity=0.9
+let upTimerId
+let downTimerId
+let isJumping=true
+let isGoingLeft=false
+let isGoingRight=false
+let leftTimerId
+let rightTimerId
+
+
 
     function createDoodler(){
         grid.appendChild(doodler)
@@ -42,11 +53,58 @@ grid.appendChild(visual)
             let platGap=600/platformCount
             let newPlatBottom=100+i*platGap
             let newPlatform=new Platform(newPlatBottom)
-            platform
+            platform.push(newPlatform)
         }
 
 
     }
+
+
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+              platform.bottom -= 4
+              let visual = platform.visual
+              visual.style.bottom = platform.bottom + 'px'
+    
+              if(platform.bottom < 10) {
+                let firstPlatform = platforms[0].visual
+                firstPlatform.classList.remove('platform')
+                platforms.shift()
+                console.log(platforms)
+                score++
+                var newPlatform = new Platform(600)
+                platforms.push(newPlatform)
+              }
+          }) 
+        }
+        
+      }
+
+
+      function createDoodler{
+
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodlerLeftSpace=platforms[0].left
+        doodler.style.left=doodlerLeftSpace+'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+
+      }
+
+
+      function fall(){
+
+        isJumping=false
+        
+
+
+
+
+
+
+      }
+
 
 
 function start(){
